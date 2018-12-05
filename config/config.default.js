1f2f189ac5636b8700b4b278ec53d6b715cee7e2
ziqiong <ziqiong.xie@dianrong.com>
2018-12-05 14:52:25 +0800

    learn

--- config/config.default.js
+++ config/config.default.js
@@ -1,1 +1,22 @@
-exports.keys = "dsfdsfds"
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+  config.keys = "dsfdsfds"
+  // view engine
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'egg-sequelize-doc-default',
+  };
+
+  return config;
+};
