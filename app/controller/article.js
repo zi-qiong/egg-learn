608831e64ff36136f8d4cd85d685ae8753dbd655
leiyuanyuan <leiyuanyuan@variflight.com>
2018-12-07 18:10:45 +0800

    文章页面静态样式和html架构

--- app/controller/article.js
+++ app/controller/article.js
@@ -3,7 +3,13 @@ const Controller = require('egg').Controller;
 class ArtcleController extends Controller {
     async index() {
         const menu = 'article'
-        await this.ctx.render('article/index.tpl', {menu})
+        const dataList = {
+            list: [
+              { id: 1, title: 'this is news 1', url: '/article/1',desc:'这是文章1',time:'2018-2-3',author:'www'  },
+              { id: 2, title: 'this is news 2', url: '/article/2',desc:'这是文章2',time:'2018-2-3',author:'www' }
+            ]
+        };
+        await this.ctx.render('article/index.tpl', {menu,dataList})
     }
 }
 
--- app/view/article/index.tpl
+++ app/view/article/index.tpl
@@ -1,1 +1,16 @@
-{% include '../layout/index.tpl' %}
+{% extends '../layout/index.tpl' %}
+{% block content %}
+<div class="listView">
+    <ul>
+        <li class="list-item">
+            <h2>{{item.title}}</h2>
+            <div class="info">
+                <span>作者</span>
+                <span>创建时间</span>
+            </div>
+            <div class="desc">一般我们用vue的时候，有很多好用的插件，我们npm安装一下就可以用了，但是像表单验证这种的很难找到自己心仪的插件，和自己的网站交互也不一样，还需要修改一下，而且文件很大，不如自己手写一个了，只要自己需要的功能，可能写的不好，提出意见</div>
+            <a class="btn">Read More</a>
+        </li>
+    </ul>
+</div>
+{% endblock %}
